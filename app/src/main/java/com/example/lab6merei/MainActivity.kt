3ed37a4e80c6b53a785c022024c87d716bf0756f
mora-suekko <manarbekov2006@gmail.com>
2025-03-25 11:02:05 +0500

    final commit

--- app/src/main/java/com/example/lab6merei/MainActivity.kt
+++ app/src/main/java/com/example/lab6merei/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val items = listOf("Элемент 1", "Элемент 2", "Элемент 3", "Элемент 4")
+        val items = listOf("Элемент 1", "Элемент 2", "Элемент 3", "Элемент 4", "Элемент 5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18")
         recyclerView.adapter = MyAdapter(items) { item ->
             Toast.makeText(this, "Вы выбрали: $item", Toast.LENGTH_SHORT).show()
         }
